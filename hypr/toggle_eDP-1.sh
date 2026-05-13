7daeec79512fc5b5a8d836bd8c79b2463ab8fc5d
KaiPenazzi <k.penazzi@web.de>
2026-05-13 10:16:19 +0200

    restart hyprpaper and waybar after turning eDP-1 off

--- hypr/toggle_eDP-1.sh
+++ hypr/toggle_eDP-1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if hyprctl monitors | grep -q "DP-1"; then
+if hyprctl monitors | grep -q "eDP-1"; then
     hyprctl keyword monitor "eDP-1,disable"
 else
     hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1.25"
 fi
+
+pkill hyprpaper
+hyprpaper &
+
+pkill waybar
+waybar &
